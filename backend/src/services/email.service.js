d1a00740d915dfbcbe7f65c3d0a48327c59670e7
Amrit Pandey <amritpandey9695@gmail.com>
2026-08-15 10:33:51 +0530

    added resend domain

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -12,7 +12,7 @@ const sendEmail = async ({ to, subject, html }) => {
   try {
     // console.log("EMAIL TO:", to);
     const { data, error } = await resend.emails.send({
-      from: "Beauty Salon <noreply@bahrain.v1.beautisalon.online>", 
+      from: "Beauty Salon Bahrain <noreply@beautisalon.online>", 
       to,
       subject,
       html,
